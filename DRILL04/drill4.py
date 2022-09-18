d4ba7e4a7ffe332920893cf9b684e4b9a86fad2d
Teterschaft <gofla2242@tukorea.ac.kr>
2022-09-18 15:37:32 +0900

    DRILL04 완성
    2021146031 이정범

---
+++ DRILL04/drill4.py
@@ -0,0 +1,33 @@
+import turtle
+
+count = 4
+
+while count > 0:
+    turtle.forward(500); turtle.left(90)
+    count -= 1
+
+turtle.forward(100); turtle.left(90)
+turtle.forward(500); turtle.right(90)
+
+turtle.forward(100); turtle.right(90)
+turtle.forward(500); turtle.left(90)
+
+turtle.forward(100); turtle.left(90)
+turtle.forward(500); turtle.right(90)
+
+turtle.forward(100); turtle.right(90)
+turtle.forward(500); turtle.left(90)
+
+turtle.home()
+
+turtle.left(90); turtle.forward(100)
+turtle.right(90); turtle.forward(500)
+
+turtle.left(90); turtle.forward(100)
+turtle.left(90); turtle.forward(500)
+
+turtle.right(90); turtle.forward(100)
+turtle.right(90); turtle.forward(500)
+
+turtle.left(90); turtle.forward(100)
+turtle.left(90); turtle.forward(500)
